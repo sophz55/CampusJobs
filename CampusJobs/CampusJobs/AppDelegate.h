1dbe94ecfc551108a48c8c63f018b8aced633979
Sophia Zheng <szheng@fb.com>
2018-07-06 10:42:34 -0700

    init

---
+++ CampusJobs/CampusJobs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CampusJobs
+//
+//  Created by Sophia Zheng on 7/6/18.
+//  Copyright © 2018 So What. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CampusJobs/CampusJobs/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CampusJobs
+//
+//  Created by Sophia Zheng on 7/6/18.
+//  Copyright © 2018 So What. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
