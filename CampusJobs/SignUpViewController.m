69b87109c1bf9d36fece62af48097bbbe20cbd9f
Sophia Zheng <szheng@fb.com>
2018-07-16 14:10:05 -0700

    adding register user and setting up user properties

--- CampusJobs/SignUpViewController.m
+++ CampusJobs/SignUpViewController.m
@@ -19,6 +19,66 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)registerUser {
+    PFUser *newUser = [PFUser user];
+    
+    newUser.username = @"username";
+    newUser.email = @"email";
+    newUser.password = @"password";
+    newUser[@"address"] = @"address";
+    newUser[@"name"] = @"full name";
+    newUser[@"rating"] = @5;
+    newUser[@"profileImageFile"] = [self getPFFileFromImage:[UIImage imageNamed:@"image_placeholder"]];
+    newUser[@"creditCardNumber"] = @00000000000;
+    newUser[@"numberJobsCompleted"] = @0; // number of jobs completed as job taker
+    newUser[@"conversations"] = [[NSMutableArray alloc] init];
+    
+    
+    if (![newUser.email isEqual:@""] && ![newUser[@"name"] isEqual:@""] && ![newUser.username isEqual:@""] && ![newUser.password isEqual:@""]) {
+        // call sign up function on the object
+        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+            if (error != nil) {
+                NSLog(@"Error: %@", error.localizedDescription);
+                [self callAlertWithTitle:@"Error" alertMessage:[NSString stringWithFormat:@"%@",error.localizedDescription]];
+            } else {
+                NSLog(@"User registered successfully");
+                [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+            }
+        }];
+    } else {
+        [self callAlertWithTitle:@"Cannot Sign Up" alertMessage:@"No fields can be blank"];
+        NSLog(@"Did not register user");
+    }
+}
+
+- (void)callAlertWithTitle:(NSString *)title alertMessage:(NSString *)message {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:(UIAlertControllerStyleAlert)];
+    
+    // create OK action
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        // handle response here.
+    }];
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:^{}];
+}
+
+- (PFFile *)getPFFileFromImage: (UIImage * _Nullable)image {
+    
+    // check if image is not nil
+    if (!image) {
+        return nil;
+    }
+    
+    NSData *imageData = UIImagePNGRepresentation(image);
+    // get image data and check if that is not nil
+    if (!imageData) {
+        return nil;
+    }
+    
+    return [PFFile fileWithName:@"image.png" data:imageData];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
