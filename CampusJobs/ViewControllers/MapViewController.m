add370119e7a531788d2bdb2e38a571204f07516
Sophia Khezri <skhezri@fb.com>
2018-07-23 15:32:42 -0700

    Completed MapViewController with slide bar and auto-zoom

--- CampusJobs/ViewControllers/MapViewController.m
+++ CampusJobs/ViewControllers/MapViewController.m
@@ -12,7 +12,9 @@
 @interface MapViewController () <CLLocationManagerDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (weak, nonatomic) IBOutlet UISlider *radiusSliderBar;
+@property (weak, nonatomic) IBOutlet UILabel *desiredRadiusLabel;
 @property (strong, nonatomic) CLLocationManager * locationManager;
+@property (strong, nonatomic) CLLocation * userLocation;
 @end
 
 @implementation MapViewController
@@ -28,6 +30,8 @@
     if([self.locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]){
         [self.locationManager requestWhenInUseAuthorization];
     }
+    //set user location
+    self.userLocation=self.locationManager.location;
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -35,27 +39,38 @@
 - (IBAction)didTapNextButton:(id)sender {
     [self performSegueWithIdentifier:@"mapToTabBarSegue" sender:nil];
 }
--(void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation*)userLocation {
-    [self.mapView setRegion:MKCoordinateRegionMake(userLocation.coordinate, MKCoordinateSpanMake(.11f, .11f)) animated:YES];
-    MKCircle *circle = [MKCircle circleWithCenterCoordinate:userLocation.coordinate radius:1500];
-    [self.mapView addOverlay:circle];
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation*)userLocation {
+    [self.mapView setRegion:MKCoordinateRegionMake(userLocation.coordinate, MKCoordinateSpanMake(.09f, .09f)) animated:YES];
 }
--(MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(nonnull id<MKOverlay>)overlay{
+- (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(nonnull id<MKOverlay>)overlay{
     MKCircleRenderer * circleRenderer=[[MKCircleRenderer alloc]initWithOverlay:overlay];
     circleRenderer.strokeColor=[UIColor grayColor];
     circleRenderer.fillColor=[UIColor grayColor];
-    circleRenderer.alpha=.04f;
+    circleRenderer.alpha=.5f;
     return circleRenderer;
-    
+}
+- (IBAction)slideBarValueChanged:(id)sender {
+    self.desiredRadiusLabel.text=[NSString stringWithFormat:@"%f",self.radiusSliderBar.value];
+    [self createBoundaryWithRadius:self.radiusSliderBar.value];
+}
+- (void)createBoundaryWithRadius:(float)radius{
+    [self.mapView removeOverlays: self.mapView.overlays];
+    MKCircle *circle = [MKCircle circleWithCenterCoordinate:self.userLocation.coordinate radius:(self.radiusSliderBar.value) * 1609.34];
+    [self.mapView addOverlay:circle];
+    //Zoom out or in depending on the selected radius
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(self.userLocation.coordinate, (radius*1609.34)*3, (radius*1609.34)*3);
+    MKCoordinateRegion adjustedRegion = [self.mapView regionThatFits:viewRegion];
+    [self.mapView setRegion:adjustedRegion animated:YES];
+    self.mapView.showsUserLocation = YES;
 }
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
